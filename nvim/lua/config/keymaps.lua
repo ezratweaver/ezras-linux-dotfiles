5d94e7c9473704f2d5139f9eff2bc3744fa0fccd
ezratweaver <ezratweaver@gmail.com>
2024-07-15 06:35:08 -0400

    Add function to not copy newlines

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -3,6 +3,11 @@
 -- Add any additional keymaps here
 local setKeyMap = vim.keymap.set
 local deleteKeyMap = vim.keymap.del
-local opts = { silent = true }
+local opts = { silent = true, noremap = true }
+local function yank_until_newline()
+  vim.cmd("normal! y/\n")
+end
 
 vim.api.nvim_set_option("clipboard", "unnamedplus")
+
+setKeyMap("n", "<S-4>", ":lua yank_until_newline()<CR>", opts)
