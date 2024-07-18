3ca1aec3d7a816b43ad23d19380c53e8d8feb312
ezratweaver <ezratweaver@gmail.com>
2024-07-18 09:53:29 -0400

    Add update_in_insert on neovim config

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -2,6 +2,10 @@ return {
   {
     "neovim/nvim-lspconfig",
     opts = {
+      ---@type vim.diagnostic.Opts
+      diagnostics = {
+        update_in_insert = true,
+      },
       servers = {
         prismals = {},
         clangd = {},
