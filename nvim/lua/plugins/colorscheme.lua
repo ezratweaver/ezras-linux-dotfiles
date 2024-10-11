441811b119f1bfb63fa232ae36aa1b4fba06fb71
Ezra Weaver <ezratweaver@gmail.com>
2024-10-10 20:31:19 -0400

    Change default colorscheme

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,4 +1,4 @@
 return {
   { "AlexvZyl/nordic.nvim", "navarasu/onedark.nvim", "rebelot/kanagawa.nvim", "Mofiqul/vscode.nvim" },
-  { "LazyVim/LazyVim", opts = { colorscheme = "vscode" } },
+  { "LazyVim/LazyVim", opts = { colorscheme = "onedark" } },
 }
