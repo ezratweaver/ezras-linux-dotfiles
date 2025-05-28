1d93c5b8f59c4d8113ec2952739ca6e6a11b0c39
Ezra Weaver <ezratweaver@gmail.com>
2025-05-28 11:44:25 -0400

    Update nvim

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -3,6 +3,7 @@ return {
     "neovim/nvim-lspconfig",
     opts = {
       ---@type vim.diagnostic.Opts
+      inlay_hints = { enabled = false },
       diagnostics = {
         update_in_insert = true,
       },
